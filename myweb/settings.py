496c0c453436d1914ac25c2e15ea646d71ff7682
azisyulianas <azis3111@gmail.com>
2023-07-24 20:22:23 +0700

    update static folder

--- myweb/settings.py
+++ myweb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)xxam=r^y_ab^1m54xyf27x!h6m#%llt2l7@@!_1qee@b=6l-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
