dec8d7da63ee6502f6399fd98ed165c756c94942
azisyulianas <azis3111@gmail.com>
2023-07-24 19:32:27 +0700

    update setting.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway', 
+        'USER': 'postgres',
+        'PASSWORD': 'ucpWEEzzWOMrt3qOvpZ4',
+        'HOST': 'containers-us-west-157.railway.app', 
+        'PORT': '6423',
     }
 }
 
