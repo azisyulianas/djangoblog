a105d2f2863dd89916cfa480a38cdd106b14d854
azisyulianas <azis3111@gmail.com>
2023-07-25 10:56:26 +0700

    add costume_tags.py

--- myweb/settings.py
+++ myweb/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic',
+    'users.templatetags.custom_tags',
     'blog',
     'users',
 ]
